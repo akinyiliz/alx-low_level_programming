dd79d7a8cec760ce30e5b3cfc2ec57e67487a137
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-08 12:09:59 -0700

    combinatoins of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	for (y = 48; y < 58; y++)
+	{
+		putchar(y);
+	}
+	if (x == y || y <- x)
+	{
+		putchar(34);
+		putchar(' ');
+	}
+	else if (x == 56 && y == 57)
+	{
+		
+	}
+	putchar('\n');
+	return (0);
+}
