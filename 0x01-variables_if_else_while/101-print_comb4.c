69b8c9906243acb4557d43660e3d7031e1df4a6a
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-08 21:24:02 -0700

    possible combinations of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints combination of 3 digits
+ * main - prints all possible combinations of 3 digits
  *
  * Return: Always 0 (Success)
  */
@@ -10,31 +10,25 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 48; x <= 57; x++)
+	for (x = 48; x < 58; x++)
 	{
-	for (y = 48; x <= 57; y++)
-	{
-	for (z = 48; z <= 57; z++)
-	{
-	if (x == y || y <= x || x == z || y == z || z <= y)
-	{
-	}
-	else
-	{
-		putchar(x);
-		putchar(y);
-		putchar(z);
-	if (x == 55 && y == 56 && z == 57)
-	{
-	}
-	else
-	{
-		putchar(44);
-		putchar(' ');
-	}
-	}
-	}
-	}
+		for (y = 49; y < 58; y++)
+		{
+			for (z = 50; z < 58; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
