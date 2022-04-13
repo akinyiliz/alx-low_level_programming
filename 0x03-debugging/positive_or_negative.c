d6273be4759407acbc2685c2f507094ba0ee59a0
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-13 09:33:10 +0000

    0 is zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests function that prints if 0 is zero
+ * void has no return value
+ */
+void positive_or_negative(int i)
+{
+	i = 0;
+	positive_or_negative(i);
+}
