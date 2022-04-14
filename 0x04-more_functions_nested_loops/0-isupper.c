fcfbc6d1c433080f02427904ab24d305e969bddc
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-14 09:38:11 +0000

    uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"                                                 
+                                                                  
+/**                                                               
+ * _isupper - checks if a character is uppercase                  
+ *                                                                
+ * Return: 0                                                      
+ */                                                               
+int _isupper(int c)                                               
+{                                                                 
+        return (c >= 65 && c <= 90);                              
+}
