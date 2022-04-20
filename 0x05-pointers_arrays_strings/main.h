a0ae56a5cfa7052cc36850be9075ce816610f037
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-20 21:03:12 +0000

    string to an integer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -32,3 +32,6 @@ void print_array(int *a, int n);
 
 /*function that copies the string pointed to by src, including the terminating null byte(\0), to the buffer pointed to by dest*/
 char *_strcpy(char *dest, char *src);
+
+/* function that convert a string to an integer*/
+int _atoi(char *s);
