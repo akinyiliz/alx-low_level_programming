24e5bc66e2c95572602cae13e20289f8fc8ade24
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-23 13:40:05 +0000

    changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,11 +9,11 @@ char *string_toupper(char *s)
 {
 	int count = 0;
 
-	while(*(s + count) != '\0')
+	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
 		{
-			*(s + count) = *(s + count) -32;
+			*(s + count) = *(s + count) - 32;
 		}
 		count++;
 	}
