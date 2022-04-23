690c11a3a080a661a7b659c44925aaf19b713073
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-23 11:41:49 +0000

    function prototypes

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+/*prints _putchar*/
+int _putchar (char c);
+
+/* function that concatenates 2 strings*/
+char *_strcat(char *dest, char *src);
+
+/* function concatenates two strings*/
+char *_strncat(char *dest, char *src, int n);
+
+/*function that copies a string*/
+char *_strncpy(char *dest, char *src, int n);
+
+/*function that compares two strings*/
+int _strcmp(char *s1, char *s2);
+
+/* function that reverses the content of an array*/
+void reverse_array(int *a, int n);
+
+/*funtion that changes all lowercase letters*/
+char *string_toupper(char *);
