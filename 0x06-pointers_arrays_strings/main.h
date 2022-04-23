1dc492b00c921552b2f7b2caaf914a0fb47d007d
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-23 12:10:26 +0000

    function prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -18,3 +18,21 @@ void reverse_array(int *a, int n);
 
 /*funtion that changes all lowercase letters*/
 char *string_toupper(char *);
+
+/*function that capitalizes all words of a string*/
+char *cap_string(char *);
+
+/*function that encodes a string into 1337*/
+char *leet(char *);
+
+/*function that encodes a string using rot13*/
+char *rot13(char *);
+
+/*function that prints an integer*/
+void print_number(int n);
+
+/*function that adds two numbers*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+/*function that prints a buffer*/
+void print_buffer(char *b, int size);
