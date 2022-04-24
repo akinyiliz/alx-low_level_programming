19bd718c8234c7cd07c721ef0ed72f5a963dd986
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-04-24 06:24:22 -0700

    prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,33 @@
 #include "main.h"
-/**
- * print_number - prints an integer
- * @n: integer
- * void has no return value
- */
 
+/**
+ * print_number - print an integer
+ * @n: input integer
+ * Return: void has no return value
+ */
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+	unsigned int m, d, count;
 
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+	d = m;
+	count = 1;
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
