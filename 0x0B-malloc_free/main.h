fcc3dc0e43818af8def9a50246569ae1e4fd568a
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-05-06 12:00:35 +0000

    creates an array of chars and initializes it with a specific char

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
 /*function that creates an array of chars, and initializes it with a specific char*/
-cahr *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c);
 
 /*function that returns a pointer to a newly allocated space in memory*/
 char *_strdup(char *str);
