23cbb4c46d8a5cf99947da0f97da59a0d39045d2
Elizabeth Odhiambo <lizzieke84@gmail.com>
2022-05-28 09:06:14 +0000

    task 3 completed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-
 	if (*head == NULL)
 		*head = new;
 
@@ -27,7 +26,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		last = *head;
 		while (last->next != NULL)
 			last = last->next;
-		last->new = new;
+		last->next = new;
 	}
 	return (*head);
 }
