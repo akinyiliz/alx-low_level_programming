ea32bc282206bcfca209734f6bfaebef8a88647b
akinyiliz <lizzieke84@gmail.com>
2022-11-03 03:05:55 +0300

    update file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,7 +3,6 @@
 /**
  * linear_search - searches for a value in an array of
  * integers using the Linear search algorithm
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
@@ -15,7 +14,6 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
